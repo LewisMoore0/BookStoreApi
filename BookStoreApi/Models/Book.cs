786724ac0556845a1316eee97eec83e30b4f2984
Lewis Moore <lewismooredev@gmail.com>
2023-05-05 11:10:32 +0200

    Initialiswe Book Model

--- BookStoreApi/Models/Book.cs
+++ BookStoreApi/Models/Book.cs
@@ -6,7 +6,18 @@ namespace BookStoreApi.Models
 {
 	public class Book
 	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string? Id { get; set; };
 
+		[BsonElement("Name")]
+		public string BookName { get; set; } = null!;
+
+		public decimal Price { get; set; }
+
+		public string Category { get; set; } = null!;
+
+		public string Author { get; set; } = null!;
 
     }
 }
