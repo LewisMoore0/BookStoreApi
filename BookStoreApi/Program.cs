7d5bbda019f38de7b76d2b6b7446ebe7a805001d
Lewis Moore <lewismooredev@gmail.com>
2023-05-05 12:33:08 +0200

    Add book controller

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -1,9 +1,12 @@
 ﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 // Add services to the container.
 
